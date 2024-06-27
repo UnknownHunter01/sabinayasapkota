40ee4fef168cab6b4f551a3bc9e28824e9091b05
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-06-27 20:44:43 +0545

    33

--- js/refresh.js
+++ js/refresh.js
@@ -1,33 +1,5 @@
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-    console.log("Cookie set: " + name + "=" + value);
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function checkRefresh() {
-    var refreshCookie = getCookie("hasRefreshed");
-    console.log("Check cookie: " + refreshCookie);
-    if (!refreshCookie) {
-        setCookie("hasRefreshed", "true", 1); // set cookie to expire in 1 day
-        console.log("Redirecting to #home section");
-        window.location.href = window.location.origin + window.location.pathname + "#home";
-    } else {
-        console.log("No redirect needed, cookie found.");
+window.onload = function() {
+    if (!window.location.search.includes('redirected=true')) {
+        window.location.href = 'https://www.sabinayasapkota.com.np/#home?redirected=true';
     }
 }
