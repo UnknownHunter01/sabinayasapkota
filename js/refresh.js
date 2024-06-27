73322b65f24a0ad48d5fefb583112ab6833265a3
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-06-27 19:26:17 +0545

    33

---
+++ js/refresh.js
@@ -0,0 +1,28 @@
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function checkRefresh() {
+    var refreshCookie = getCookie("hasRefreshed");
+    if (!refreshCookie) {
+        setCookie("hasRefreshed", "true", 1); // set cookie to expire in 1 day
+        window.location.replace("https://www.sabinayasapkota.com.np/#home");
+    }
+}
