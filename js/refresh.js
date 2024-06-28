0e86847609f21b29034b6c023905d835f5838f14
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-06-28 18:45:12 +0545

    33

--- js/refresh.js
+++ js/refresh.js
@@ -1,5 +1,4 @@
+
 window.onload = function() {
-    if (!window.location.search.includes('redirected=true')) {
-        window.location.href = 'https://www.sabinayasapkota.com.np/#home?redirected=true';
-    }
-}
+    window.location.href = "#home";
+};
