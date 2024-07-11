d42eff48ee88ffce5407570beec773cabcc42a66
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-07-11 21:28:09 +0545

    ??

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ self.addEventListener('install', event => {
         return cache.addAll([
           '/',
           '/index.html',
-          '/styles.css',
-          '/script.js',
+          'css/styles.css',
+          'js/script.js',
           'img/profile.png',
           'img/profile.png'
         ]);
