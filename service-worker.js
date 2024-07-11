6487691eb20af3dd9d82b7b7d6f09372ab50823c
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-07-11 21:38:36 +0545

    ??

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('v1').then(cache => {
-        return cache.addAll([
-          '/',
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('your-cache-name').then(function(cache) {
+      return cache.addAll([
+        '/',
           '/index.html',
           'css/styles.css',
           'js/script.js',
           'img/profile.png',
           'img/profile.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
