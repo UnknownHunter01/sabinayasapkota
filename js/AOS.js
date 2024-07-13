69dee7f0a1505438c7fb8aa5101c94da104c1749
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-07-13 21:45:44 +0545

    ?

---
+++ js/AOS.js
@@ -0,0 +1,7 @@
+    AOS.init({
+        offset: 200,  // Offset (in px) from the original trigger point
+        duration: 600,  // Duration of the animation
+        easing: 'ease-in-out',  // Easing function
+        delay: 100,  // Delay before the animation starts
+        once: false  // Animation will repeat every time you scroll up and down
+    });
