9d3115ebd9235a782e12d4786a9c3fe02fc2e2fe
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-07-13 21:49:37 +0545

    ?

--- js/AOS.js
+++ js/AOS.js
@@ -1,6 +1,6 @@
     AOS.init({
         offset: 200,  // Offset (in px) from the original trigger point
-        duration: 600,  // Duration of the animation
+        duration: 1000,  // Duration of the animation
         easing: 'ease-in-out',  // Easing function
         delay: 100,  // Delay before the animation starts
         once: false  // Animation will repeat every time you scroll up and down
