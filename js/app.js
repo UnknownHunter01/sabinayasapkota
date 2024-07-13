03c4a3a76189754aed195e6d66197192fb981653
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-07-13 16:08:10 +0545

    ?

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('ServiceWorker registration failed: ', error);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
 }
