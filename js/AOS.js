90a6ed1748499f1ed690c6175e4a5968bb394b33
Sabinaya Sapkota <sabinayasapkotavenom@gmail.com>
2024-08-06 18:36:23 +0545

    ??

--- js/AOS.js
+++ js/AOS.js
@@ -26,3 +26,30 @@ function updateSlider() {
 
 
 
+// Get the modal
+var modal = document.getElementById('modal');
+
+// Get the image and insert it inside the modal
+var modalImg = document.getElementById("modal-img");
+
+document.querySelectorAll('.cert-img').forEach(item => {
+    item.addEventListener('click', event => {
+        modal.style.display = "block";
+        modalImg.src = event.target.src;
+    });
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+// Close modal when clicking outside of the image
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
